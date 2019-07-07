2a73e4db01fdee9fb30edd1dc21c0e82296884e1
LiuTalkweb <809489286@qq.com>
2019-07-07 17:10:37 +0800

    aa

---
+++ servletDemo/src/main/java/com/study/controller/Demo.java
@@ -0,0 +1,57 @@
+package com.study.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Demo
+ */
+public class Demo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Demo() {
+        super();
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println(this.getClass()+" init");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println(this.getClass()+" destroy ");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("调用get方法");
+		request.getRequestDispatcher("demo1.jsp").forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("调用 doPost 方法");
+		/*response.setContentType("text/plain");
+		response.getWriter().write(" Served at: "+request.getContextPath());*/
+		
+		doGet(request, response);
+		
+	}
+
+}
